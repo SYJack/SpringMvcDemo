3f66a9519c49358cdee55837607a33f6fa702f03
jack <jack@jack>
2018-01-17 23:50:21 +0800

    提交BaseService

---
+++ src/main/java/org/jack/sy/service/BaseService.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package org.jack.sy.service;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+/**
+ * @author jack
+ *
+ */
+public interface BaseService<T,Serializable> {
+	
+	public int insert(T record);
+
+	public int insertSelective(T record);
+
+	public T selectByPrimaryKey(String id);
+
+	public int updateByPrimaryKey(T record);
+
+	public int updateByPrimaryKeySelective(T record);
+
+	public int deleteByPrimaryKey(String id);
+
+	public List<T> findTop(int top, String statementKey, Object parameter)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
+
+	public T findTopOne(String statementKey, Object parameter)
+			throws IllegalAccessException, InvocationTargetException, NoSuchMethodException;
+}
